a7c119435d539b794c12134ccdb0c83150af13df
Emil van der Wehle Jörn Lynge <emil5923@edu.campusvejle.dk>
2018-09-17 09:24:43 +0200

    Made the solution and the projects that I think are nessecary to complete the assignment.

--- EmilLynge.KlarupSportsBooking.DAL/Properties/AssemblyInfo.cs
+++ EmilLynge.KlarupSportsBooking.DAL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmilLynge.KlarupSportsBooking.Business")]
+[assembly: AssemblyTitle("EmilLynge.KlarupSportsBooking.DAL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EmilLynge.KlarupSportsBooking.Business")]
+[assembly: AssemblyProduct("EmilLynge.KlarupSportsBooking.DAL")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("65cee9a3-d205-4654-b099-d12cf68cb0a7")]
+[assembly: Guid("e678213f-8c81-402d-8e9b-dd44f8101ca2")]
 
 // Version information for an assembly consists of the following four values:
 //
--- EmilLynge.KlarupSportsBooking.Server/Program.cs
+++ EmilLynge.KlarupSportsBooking.Server/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmilLynge.KlarupSportsBooking.Business
+namespace EmilLynge.KlarupSportsBooking.Server
 {
     class Program
     {
